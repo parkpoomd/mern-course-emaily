19b5ab4f41a72f0ed117c872b0ba0b8a12a1c7bc
parkpoom <parkpoom.d@gmail.com>
2018-01-13 22:38:18 +0700

    update .gitignore

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js - don't commit this!!!
+module.exports = {
+  googleClientID: '341032431907-4ca66urcdavhvft0fth23p461jqbvvf7.apps.googleusercontent.com',
+  googleClientSecret: 'nyq0DQKOuB5erk88lOudiEgp',
+  mongoURI: 'mongodb://parkpoom:password@ds155577.mlab.com:55577/emaily-dev',
+  cookieKey: '1q2w3e4r'
+};
