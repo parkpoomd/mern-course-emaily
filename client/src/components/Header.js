3781d2222e8152963d673fcc56c1b47ef2eff84c
parkpoom <parkpoom.d@gmail.com>
2018-01-14 20:04:17 +0700

    style header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
+      <nav className="#e53935 red darken-1">
+        <div className="nav-wrapper container">
           <a className="left brand-logo">Emaily</a>
           <ul className="right hide-on-med-and-down">
             <li><a>Login With Google</a></li>
