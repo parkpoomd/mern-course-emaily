0956120ebdd5b69b4212cc522615a846cd147841
parkpoom <parkpoom.d@gmail.com>
2018-01-15 16:15:16 +0700

    Header Content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return (
+          <li><a>Logout</a></li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav className="#e53935 red darken-1">
         <div className="nav-wrapper container">
           <a className="left brand-logo">Emaily</a>
           <ul className="right hide-on-med-and-down">
-            <li><a>Login With Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
